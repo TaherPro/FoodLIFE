fe2c318e6074638c0f136adfbf580e5b90aef605
TaherPro <alfakihtaher@gmail.com>
2025-11-21 09:51:21 -0500

    server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import donationRoutes from "./routes/donationRoutes.js";
+import requestRoutes from "./routes/requestRoutes.js";
+import errorHandler from "./middleware/errorMiddleware.js";
 
 
 dotenv.config();
@@ -10,8 +14,15 @@ const app = express();
 
 connectDB();
 
+// middleware
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/donations", donationRoutes);
+app.use("/api/requests", requestRoutes);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5007;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
