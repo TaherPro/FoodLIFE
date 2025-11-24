58dac4f6c5b2852ab6b4736718b584eb62d8f113
TaherPro <alfakihtaher@gmail.com>
2025-11-24 13:05:59 -0500

    home page banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 export default function Home() {
   return (
-    <div className="card">
-      <h1>Welcome to FoodLIFE</h1>
-      <p>Your community food donation and distribution platform.</p>
-      <p>Please login or register to continue.</p>
+    <div className="hero-full">
+      <h1>Welcome to FoodLIFE</h1><br />
+      <p><strong>Your community food donation and distribution platform..</strong></p>
+      <img 
+        src="/foodlife.png" 
+        alt="FoodLIFE Banner"
+        className="home-hero"
+      />      
+      <h5>© Made with ᥫ᭡</h5>
     </div>
+    
   );
 }
